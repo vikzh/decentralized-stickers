be616c4df9ba751ab9f9926fd63731454953fd16
vikzh <vikt.anmail@gmail.com>
2023-07-22 12:18:30 +0200

    position on bot a fixed

--- telegram-sticker-bot/index.py
+++ telegram-sticker-bot/index.py
@@ -25,12 +25,15 @@ async def main():
         return list(sticker_set.stickers)
 
     async def delete_sticker(position):
+        assert position >= 0 and position < 120
         stickers = await list_stickers()
-        await bot.delete_sticker_from_set(stickers[position].file_id)
+        await bot.delete_sticker_from_set(stickers[position].file_id, connect_timeout=160)
 
     stickers = await list_stickers()
-    for sticker in stickers:
-        print(sticker)
+    print(len(stickers))
+    await delete_sticker(0)
+    stickers = await list_stickers()
+    print(len(stickers))
 
 if __name__ == "__main__":
     asyncio.run(main())
